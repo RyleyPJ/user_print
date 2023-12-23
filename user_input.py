e4ace69915779711f36421b61377273c5d848b24
Ryley <jonesryley07@gmail.com>
2023-12-23 15:29:55 +0000

    user_interface_maincode

---
+++ user_input.py
@@ -0,0 +1,17 @@
+def get_user_input():
+    name = input("Enter your name")
+    age = input("Enter your age")
+    return name, age
+
+def print_greeting(name, age):
+    print(f"Hello {name}! You are {age} years old")
+
+# Main program
+    
+    if __name__ == "__main__":
+        # Get's user input
+        user_name, user_age = get_user_input()
+
+        # Prints personalised greeting
+
+        print_greeting(user_name, user_age)
